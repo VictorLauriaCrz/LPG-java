c8bf1a6db8d79751b04e78a6dac458b425dc187b
Victor Lauria <98828746+VictorLauriaCrz@users.noreply.github.com>
2026-04-06 20:52:20 -0300

    Add files via upload

---
+++ 07-reserva-de-carga/src/br/fiap/carga/Carga.java
@@ -0,0 +1,67 @@
+package br.fiap.carga;
+
+
+import br.fiap.cliente.Cliente;
+
+import java.util.Random;
+
+public class Carga {
+    private int id;
+    private double peso;
+    private String destino;
+    private Cliente cliente;
+
+    public Carga(String destino, Cliente cliente){
+        this.destino = destino;
+        this.cliente = cliente;
+        this.id = gerarId();
+        this.peso = gerarPeso();
+    }
+
+    private int gerarId(){
+        Random random = new Random();
+        return random.nextInt(1000, 9999);
+    }
+
+    private double gerarPeso(){
+        Random random = new Random();
+        return random.nextDouble(50, 90);
+    }
+
+    public String getDados(){
+        String aux = "";
+        aux = "10: " + id + "\n";
+        aux += "Destino: " + destino + "\n";
+        aux += "Peso: " + peso + "Kg\n";
+        aux += cliente.getDados();
+        return aux;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
---
+++ 07-reserva-de-carga/src/exemplo/Exemplo.java
@@ -0,0 +1,17 @@
+package exemplo;
+
+import static  javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        int valor1, valor2, resultado;
+
+        valor1 = parseInt(showInputDialog("informe o primeiro valor"));
+        valor2 = parseInt(showInputDialog("informe o primeiro valor"));
+
+        resultado = valor1 + valor2;
+        showMessageDialog(null, "resultado" +resultado);
+
+    }
+}
